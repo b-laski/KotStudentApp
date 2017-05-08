bc2286f67aee3f31f976f6af7a0884e3c0df5fab
DESKTOP-4D94KU7\Tajfun695 <tajfun695@gmail.com>
2017-05-09 01:13:59 +0200

    Update!

---
+++ KotStudentApp/KotStudentApp/Controls/PostPreview/PostPreview.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace KotStudentApp
+{
+    /// <summary>
+    /// Interaction logic for PostPreview.xaml
+    /// </summary>
+    public partial class PostPreview : UserControl
+    {
+        public PostPreview()
+        {
+            InitializeComponent();
+        }
+    }
+}
